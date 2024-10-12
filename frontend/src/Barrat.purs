c5a14cb4e11c1f9b7efc150482e249f2a37e8c1e
David Villasana Jiménez <davidvillasana14@gmail.com>
2024-10-12 17:09:24 -0600

    Auto-commit: updated src/Barrat.purs

--- frontend/src/Barrat.purs
+++ frontend/src/Barrat.purs
@@ -5,44 +5,81 @@ import Prelude
 import Affjax.RequestBody as RequestBody
 import Affjax.ResponseFormat as ResponseFormat
 import Affjax.Web as AX
-import Data.Argonaut.Core (jsonSingletonObject, stringify, Json)
-import Data.Argonaut.Core as DAC
-import Data.Argonaut.Encode as DAE
-import Data.Array (fold, foldl, replicate, snoc, updateAt, zip, range)
-import Data.Either (Either(..))
-import Data.Foldable (traverse_)
-import Data.Int (fromString)
-import Data.Maybe (Maybe(..), fromMaybe)
+import Data.Array (foldl, replicate, snoc, updateAt, zip, range)
+import Data.Maybe (Maybe(..))
 import Data.String (joinWith)
-import Data.Tuple (Tuple(..), fst, snd)
-import Effect.Aff (Aff)
+import Data.Tuple (Tuple(..))
 import Effect.Aff.Class (class MonadAff)
-import Effect.Class (class MonadEffect, liftEffect)
-import Effect.Console (log)
+import Effect.Class (class MonadEffect)
 import Effect.Exception.Unsafe (unsafeThrow)
+import Extras (InstructionsOutput, InstructionsSlot, instructionsComponent)
 import Halogen as H
 import Halogen.HTML as HH
 import Halogen.HTML.Events as HE
 import Halogen.HTML.Properties as HP
+import Type.Proxy (Proxy(..))
 import Web.Event.Event (Event, preventDefault)
 
-type State = { answers :: Array String }
+data BarratStage 
+  = BarratInstructions
+  | BarratForm
+
+type State =
+  { answers :: Array String
+  , stage :: BarratStage
+  }
 
 initialState :: forall i. i -> State
-initialState _ = { answers: replicate 30 "" }
+initialState _ = 
+  { answers: replicate 30 "" 
+  , stage: BarratInstructions
+  }
 
 data Action 
-  = UpdateAnswers Int String
+  = BarratInstructionsDone InstructionsOutput
+  | UpdateAnswers Int String
   | HandleSubmit Event
+  | HandleBarratDone Output
 
 data Output = BarratDone
 type Slot = forall query. H.Slot query Output Int
+
+type BarratSlot = forall query. H.Slot query Output Int
+
+type ChildSlots = 
+  ( barratInstructions :: InstructionsSlot
+  , barratForm :: BarratSlot
+  )
+
+_barratInstructions = Proxy :: Proxy "barratInstructions"
+_barratForm = Proxy :: Proxy "barratForm"
+
+render :: forall m. MonadAff m => State -> H.ComponentHTML Action ChildSlots m
+render state = case state.stage of
+  BarratInstructions -> HH.slot _barratInstructions 10 (instructionsComponent instructions) unit BarratInstructionsDone
+  BarratForm -> HH.slot _barratForm 11 barratFormComponent unit HandleBarratDone
+
+barratComponent :: forall query input m. MonadAff m => H.Component query input Output m
+barratComponent =
+  H.mkComponent
+  { initialState
+  , render
+  , eval: H.mkEval $ H.defaultEval { handleAction = barratHandler }
+  }
+
+barratHandler :: forall m. MonadEffect m => Action -> H.HalogenM State Action ChildSlots Output m Unit
+barratHandler = 
+  case _ of
+       BarratInstructionsDone _ -> H.modify_ \state -> state { stage = BarratForm }
+       HandleBarratDone _ -> H.raise BarratDone
+       _ -> pure unit
 
 handleAction :: forall m. MonadAff m => Action -> H.HalogenM State Action () Output m Unit
 handleAction action = do
   case action of
     UpdateAnswers index val -> setAnswer index val
     HandleSubmit ev -> handleSubmit ev
+    _ -> pure unit
 
 setAnswer :: forall m. MonadEffect m => Int -> String -> H.HalogenM State Action () Output m Unit
 setAnswer index val = do
@@ -58,40 +95,6 @@ handleSubmit ev = do
   _ <- H.liftAff $ AX.post ResponseFormat.ignore "/barrat"
     (Just $ RequestBody.String $ joinWith " " answers)
   H.raise BarratDone
-
-questions :: Array String
-questions = 
-  [ "1. Planifico mis tareas con cuidado"
-  , "2. Hago las cosas sin pensarlas"
-  , "3. Casi nunca me tomo las cosas a pecho (no me perturbo con facilidad)"
-  , "4. Mis pensamientos pueden tener gran velocidad (tengo pensamientos que van muy rápido en mi mente)"
-  , "5. Planifico mis viajes con antelación"
-  , "6. Soy una persona con autocontrol"
-  , "7. Me concentro con facilidad (se me hace fácil concentrarme)"
-  , "8. Ahorro con regularidad"
-  , "9. Se me hace difícil estar quieto/a durante largos períodos de tiempo"
-  , "10. Pienso las cosas cuidadosamente"
-  , "11. Planifico para tener un trabajo fijo (me esfuerzo por asegurar que tendré dinero para pagar mis gastos)"
-  , "12. Digo las cosas sin pensarlas"
-  , "13. Me gusta pensar sobre problemas complicados (me gusta pensar sobre problemas complejos)"
-  , "14. Cambio de trabajo frecuentemente (no me quedo en el mismo trabajo durante largos períodos de tiempo)"
-  , "15. Actúo impulsivamente"
-  , "16. Me aburro con facilidad tratando de resolver problemas en mi mente (me aburre pensar en algo por demasiado tiempo)"
-  , "17. Visito al médico y al dentista con regularidad"
-  , "18. Hago las cosas en el momento que se me ocurren"
-  , "19. Soy una persona que piensa sin distraerse (puedo enfocar mi mente en una sola cosa por mucho tiempo)"
-  , "20. Cambio de vivienda a menudo (me mudo con frecuencia o no me gusta vivir en el mismo sitio por mucho tiempo)"
-  , "21. Compro cosas impulsivamente"
-  , "22. Termino lo que empiezo"
-  , "23. Camino y me muevo con rapidez"
-  , "24. Resuelvo los problemas experimentando (resuelvo los problemas empleando una posible solución y viendo si funciona)"
-  , "25. Gasto en efectivo o a crédito más de lo que gano (gasto más de lo gano)"
-  , "26. Hablo rápido"
-  , "27. Tengo pensamientos extraños cuando estoy pensando (a veces tengo pensamientos irrelevantes cuando pienso)"
-  , "28. Me interesa más el presente que el futuro"
-  , "29. Me siento inquieto/a en clases o charlas (me siento inquieto/a si tengo que oír a alguien hablar durante un largo período de tiempo)"
-  , "30. Planifico el futuro (me interesa más el futuro que el presente)"
-  ]
 
 mkQuestion :: forall w. String -> Int -> HH.HTML w Action
 mkQuestion q index =
@@ -141,8 +144,8 @@ renderBarrat _ =
       ]
     ]
 
-barratComponent :: forall input query m. MonadAff m => H.Component query input Output m
-barratComponent =
+barratFormComponent :: forall input query m. MonadAff m => H.Component query input Output m
+barratFormComponent =
   H.mkComponent
     { initialState
     , render: renderBarrat
@@ -150,3 +153,45 @@ barratComponent =
        { handleAction = handleAction
        }
     }
+
+instructions :: String
+instructions =
+  "Las personas son diferentes en cuanto a la forma en que se comportan y " <>
+  "piensan en distintas situaciones. Ésta es una  prueba para medir algunas " <>
+  "de las formas en que usted actúa y piensa. No se detenga demasiado tiempo " <>
+  "en ninguna de las oraciones. Responda rápida y honestamente."
+
+questions :: Array String
+questions = 
+  [ "1. Planifico mis tareas con cuidado"
+  , "2. Hago las cosas sin pensarlas"
+  , "3. Casi nunca me tomo las cosas a pecho (no me perturbo con facilidad)"
+  , "4. Mis pensamientos pueden tener gran velocidad (tengo pensamientos que van muy rápido en mi mente)"
+  , "5. Planifico mis viajes con antelación"
+  , "6. Soy una persona con autocontrol"
+  , "7. Me concentro con facilidad (se me hace fácil concentrarme)"
+  , "8. Ahorro con regularidad"
+  , "9. Se me hace difícil estar quieto/a durante largos períodos de tiempo"
+  , "10. Pienso las cosas cuidadosamente"
+  , "11. Planifico para tener un trabajo fijo (me esfuerzo por asegurar que tendré dinero para pagar mis gastos)"
+  , "12. Digo las cosas sin pensarlas"
+  , "13. Me gusta pensar sobre problemas complicados (me gusta pensar sobre problemas complejos)"
+  , "14. Cambio de trabajo frecuentemente (no me quedo en el mismo trabajo durante largos períodos de tiempo)"
+  , "15. Actúo impulsivamente"
+  , "16. Me aburro con facilidad tratando de resolver problemas en mi mente (me aburre pensar en algo por demasiado tiempo)"
+  , "17. Visito al médico y al dentista con regularidad"
+  , "18. Hago las cosas en el momento que se me ocurren"
+  , "19. Soy una persona que piensa sin distraerse (puedo enfocar mi mente en una sola cosa por mucho tiempo)"
+  , "20. Cambio de vivienda a menudo (me mudo con frecuencia o no me gusta vivir en el mismo sitio por mucho tiempo)"
+  , "21. Compro cosas impulsivamente"
+  , "22. Termino lo que empiezo"
+  , "23. Camino y me muevo con rapidez"
+  , "24. Resuelvo los problemas experimentando (resuelvo los problemas empleando una posible solución y viendo si funciona)"
+  , "25. Gasto en efectivo o a crédito más de lo que gano (gasto más de lo gano)"
+  , "26. Hablo rápido"
+  , "27. Tengo pensamientos extraños cuando estoy pensando (a veces tengo pensamientos irrelevantes cuando pienso)"
+  , "28. Me interesa más el presente que el futuro"
+  , "29. Me siento inquieto/a en clases o charlas (me siento inquieto/a si tengo que oír a alguien hablar durante un largo período de tiempo)"
+  , "30. Planifico el futuro (me interesa más el futuro que el presente)"
+  ]
+
