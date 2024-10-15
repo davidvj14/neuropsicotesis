11b536a15c149272a3ef5e0eddb31e63e65cb91b
David Villasana Jiménez <davidvillasana14@gmail.com>
2024-10-14 18:45:52 -0600

    Auto-commit: updated src/Main.purs

--- frontend/src/Main.purs
+++ frontend/src/Main.purs
@@ -1,13 +1,16 @@
 module Main where
 
-import Coordinator (mainComponent)
-import Prelude (unit, Unit, bind)
-
+import Coordinator (mainComponent, stageFromMbStr)
 import Effect (Effect)
+import Extras (readCookie)
+import Halogen as H
 import Halogen.Aff as HA
 import Halogen.VDom.Driver (runUI)
+import Prelude
 
 main :: Effect Unit
 main = HA.runHalogenAff do
+  stageCookie <- H.liftEffect $ readCookie "stage"
+  let stage = stageFromMbStr stageCookie
   body <- HA.awaitBody
-  runUI mainComponent unit body
+  runUI mainComponent stage body
